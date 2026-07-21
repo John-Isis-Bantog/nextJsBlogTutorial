5b44aff1ab1be0814f928b33b7bdd328cce1fd74
John-Isis-Bantog <bantogjohnisis@gmail.com>
2026-07-21 15:04:39 +0800

    feat: added navigation, header, and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,35 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className="h-full antialiased">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} min-h-full`}
+      >
+        <div className="min-h-dvh bg-zinc-200 text-zinc-950">
+          <div className="mx-auto flex min-h-dvh w-full max-w-275 flex-col border-t  border-zinc-300 bg-white">
+            <header className="flex h-14 items-center justify-between border-b border-zinc-200 px-4 sm:px-6">
+              <Link href="/" className="flex items-center gap-3 font-semibold">
+                <h1>Next.Js</h1>
+              </Link>
+              <nav className="flex items-center gap-4 text-sm font-medium">
+                <Link href="/" className="hover:text-zinc-950">
+                  Home
+                </Link>
+                <Link href="/posts" className="hover:text-zinc-950">
+                  Posts
+                </Link>
+              </nav>
+            </header>
+
+            <main className="flex-1 px-4 py-8 sm:px-6 sm:py-10">
+              {children}
+            </main>
+            <footer className="flex h-14 items-center border-t border-white px-4 text-s text-zinc-500 sm:px-6">
+              2026 Next Blog
+            </footer>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
